68850cd60640ac95cea04d2399cbfae59dd2e7c9
Cole <collinsmungatia@gmail.com>
2025-09-17 13:21:13 +0300

    Add files via upload

---
+++ assignment wk7/script/script.js
@@ -0,0 +1,9 @@
+const cube = document.getElementById("cube-2");
+
+function startAnimation() {
+    cube.classList.add('animate');
+}
+
+function stopAnimation() {
+    cube.classList.remove('animate');
+}
